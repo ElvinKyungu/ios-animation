657fefbc8262690fc57aa618d558a59edba068ca
elvinkyungu <elvinkyungu.75@gmail.com>
2024-06-20 16:37:56 +0200

    init project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+  import SreenLock from '@/components/SreenLock.vue';
 </script>
 
 <template>
-  <main>
-    
+  <main class="w-full h-screen bg-gray-900 flex justify-center items-center">
+    <div class="px-10 min-h-screen flex justify-center items-center">
+      <div 
+        class="
+          relative h-[600px] w-[320px] bg-black rounded-[60px] shadow-xl 
+          overflow-hidden border-[14px] border-black cursor-pointer
+        "
+      >
+        <SreenLock/>
+      </div>
+    </div>
   </main>
 </template>
+<style>
+
+</style>
